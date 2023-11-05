7b8869aa5c0c8acfaf7eac06e4c8e07559d7d2fa
Deboneil <bhattacharjeedeboneil@gmail.com>
2023-11-05 11:13:22 +0530

    taking input from user

--- sums.sh
+++ sums.sh
@@ -4,3 +4,19 @@ x=$((30))
 y=$((90))
 z=$((x+y))
 echo $z
+
+#read var_name
+#echo "the input is: " $var_name
+
+#echo enter the first number
+#read a
+
+#echo enter second number 
+#read b
+
+#result=$((a+b))
+#echo the result is : $result
+
+read -p "hows the day:  " a
+echo "User says its:" $a
+
